1f42fe59447a1dc5b0a4dfe320238641cf471b12
cryptobakkal <136384432+cryptobakkal@users.noreply.github.com>
2026-03-05 20:53:44 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:51:10.835Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T17:53:44.453Z
+Commit: Resolve null pointer exception in user service

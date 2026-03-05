349efa4a114ea85daea45cf15c25ee50ad612293
cryptobakkal <136384432+cryptobakkal@users.noreply.github.com>
2026-03-05 20:56:15 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:56:13.501Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T17:56:14.843Z
+Commit: Fix race condition in async operations

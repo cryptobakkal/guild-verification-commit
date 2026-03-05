f0a2294cfe1ec6d23527d6bfea8b975000ba7134
cryptobakkal <136384432+cryptobakkal@users.noreply.github.com>
2026-03-05 20:55:54 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:55:45.224Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T17:55:54.581Z
+Commit: Fix memory leak in event handlers

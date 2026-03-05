fcf4c38d63ba04633ea8b065f538983db0b40124
cryptobakkal <136384432+cryptobakkal@users.noreply.github.com>
2026-03-05 20:55:56 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:55:54.581Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T17:55:55.925Z
+Commit: Resolve timezone issues in date handling

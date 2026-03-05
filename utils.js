5d79670534ff70e606e9ca873d800eab6cf50080
cryptobakkal <136384432+cryptobakkal@users.noreply.github.com>
2026-03-05 20:54:13 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:54:09.113Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T17:54:13.860Z
+Commit: Resolve timezone issues in date handling

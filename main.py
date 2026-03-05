0a624c2710acfb4dbcac8f25962f3beff2fc7fbc
cryptobakkal <136384432+cryptobakkal@users.noreply.github.com>
2026-03-05 20:53:55 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:50:56.693Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T17:53:55.595Z
+Commit: Fix memory leak in event handlers

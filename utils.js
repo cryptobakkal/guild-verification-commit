5886867e2c201a8da17474fdbdb3843bc4814eba
cryptobakkal <136384432+cryptobakkal@users.noreply.github.com>
2026-03-05 20:55:38 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:55:34.176Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T17:55:38.359Z
+Commit: Patch API endpoint error handling
